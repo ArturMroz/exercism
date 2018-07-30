8937e6ceb79cfbe71c77e9cfc36a29157218135d
Artur Mroz <frost47@gmail.com>
2018-07-30 21:51:39 +0100

    added FlattenArray in C#

---
+++ csharp/flatten-array/FlattenArray.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class FlattenArray
+{
+    private static IEnumerable<object> FlattenRecursive(IEnumerable<object> array) => 
+        array.SelectMany(x => x is IEnumerable<object> subArray ? FlattenRecursive(subArray) : new[] { x });
+
+    public static IEnumerable Flatten(IEnumerable input) => 
+        FlattenRecursive(input.Cast<object>()).Where(x => x != null);
+}
