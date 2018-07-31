3acdad6541d22602d81bf85c1a5d0ff2d89bdcbc
Artur Mroz <frost47@gmail.com>
2018-07-31 19:15:59 +0100

    added BankAccount in C#

---
+++ csharp/bank-account/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class BankAccount
+{
+    private bool isOpen = false;
+    private float balance;
+    private readonly object bankAccountLock = new Object();
+
+    public void Open()
+    {
+        isOpen = true;
+    }
+
+    public void Close()
+    {
+        isOpen = false;
+    }
+
+    public float Balance
+    {
+        get
+        {
+            if (!isOpen) throw new InvalidOperationException();
+
+            return balance;
+        }
+        private set
+        {
+            if (!isOpen) throw new InvalidOperationException();
+
+            balance = value;
+        }
+    }
+
+    public void UpdateBalance(float change)
+    {
+        lock (bankAccountLock)
+        {
+            Balance += change;
+        }
+    }
+}
