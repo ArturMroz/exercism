760ff4b486661181cce4b7e6be3d2d34c75633d6
Artur Mroz <frost47@gmail.com>
2018-07-31 19:18:26 +0100

    added Acronym in C#

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var firstLettersOfWords = Regex.Matches(phrase, @"\b\w");
+        return string.Concat(firstLettersOfWords.Select(letter => letter.Value)).ToUpper();
+    }
+}
