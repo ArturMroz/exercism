956be7696f87b19ad00dc74deb50d599f76bb6b4
Artur Mroz <frost47@gmail.com>
2018-12-08 18:17:37 +0000

    added Queen Attack in JS

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -9,12 +9,15 @@ export default class Triangle {
     if (a <= 0 || b <= 0 || c <= 0) {
       throw new Error('Edge has to be bigger than 0.');
     }
+
     if (a + b <= c || a + c <= b || b + c <= a) {
       throw new Error('Illegal triangle.');
     }
+
     if (a === b && b === c) {
       return 'equilateral';
     }
+
     if (a === b || b === c || c === a) {
       return 'isosceles';
     }
