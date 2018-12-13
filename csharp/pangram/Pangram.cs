6e17b78c8804273a10b6c61d2aef1175b0a84631
Artur Mroz <frost47@gmail.com>
2018-12-13 17:10:24 +0100

    added Pangram in C#

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+  private const string _alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  public static bool IsPangram(string input) => _alphabet.All(input.ToLower().Contains);
+}
--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -3,23 +3,14 @@ using System.Linq;
 
 public static class RotationalCipher
 {
-  private static readonly char[] _az =
-    Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (Char)i).ToArray();
-
-  public static string Rotate(string text, int shiftKey)
-  {
-    return new string(text
-      .Select(ch =>
+  public static string Rotate(string text, int shiftKey) => 
+    new string(text.Select(ch =>
       {
         if (!char.IsLetter(ch)) return ch;
 
-        var isUpper = char.IsUpper(ch);
-        var valToRot = Array.IndexOf(_az, char.ToLower(ch)) + shiftKey;
-        valToRot %= _az.Length; // wrap around alphabet
-
-        var rotated = _az[valToRot];
-        return isUpper ? char.ToUpper(rotated) : rotated;
+        var start = char.IsUpper(ch) ? 'A' : 'a';
+        var offset = (ch - start + shiftKey) % 26;
+        return (char)(start + offset);
       })
       .ToArray());
-  }
 }
