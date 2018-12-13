b9b251b77883ff867838f5f9e91d468224449386
Artur Mroz <frost47@gmail.com>
2018-12-13 13:24:03 +0100

    added Darts in C#

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class Darts
+{
+  public static int Score(double x, double y)
+  {
+    var furthest = Math.Max(x, y);
+
+    if (furthest > 10) return 0;
+    if (furthest > 5) return 1;
+    if (furthest > 1) return 5;
+
+    return 10;
+  }
+}
