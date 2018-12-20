5c8b1b9dd05e730094fe4852b24a885fffdbd0f8
Artur Mroz <frost47@gmail.com>
2018-12-20 16:09:10 +0100

    updated Minesweeper and RationalNumbers in C#

--- csharp/atbash-cipher/AtbashCipher.cs
+++ csharp/atbash-cipher/AtbashCipher.cs
@@ -1,33 +1,19 @@
 using System;
 using System.Linq;
-using System.Text;
 
 public static class AtbashCipher
 {
-  private static readonly char[] _alphabet = "abcdefghijklmnopqrstuvwxyz".ToArray();
-
-  public static string Encode(string plainValue)
-  {
-    var result = new StringBuilder();
-    var stripped = plainValue.ToLower().Where(ch => char.IsLetter(ch) || char.IsNumber(ch));
-
-    foreach (var (ch, i) in stripped.Select((val, i) => (val, i + 1)))
-    {
-      result.Append(CodeChar(ch));
-      if (i % 5 == 0) result.Append(' ');
-    }
-
-    return result.ToString().Trim();
-  }
+  public static string Encode(string plainValue) =>
+    String.Concat(plainValue
+      .ToLower()
+      .Where(char.IsLetterOrDigit)
+      .Select(CodeChar)
+      .Select((ch, i) => i != 0 && i % 5 == 0 ? $" {ch}" : $"{ch}"));
 
   public static string Decode(string encodedValue) =>
-    String.Concat(encodedValue.Where(ch => ch != ' ').Select(CodeChar));
+    String.Concat(encodedValue
+      .Where(char.IsLetterOrDigit)
+      .Select(CodeChar));
 
-  private static char CodeChar(char ch)
-  {
-    if (!char.IsLetter(ch)) return ch;
-
-    var index = Array.IndexOf(_alphabet, ch);
-    return _alphabet[_alphabet.Length - index - 1];
-  }
+  private static char CodeChar(char ch) => char.IsLetter(ch) ? (char)('a' + 'z' - ch) : ch;
 }
