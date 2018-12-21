e143ea7da440fd5f26a42030214183e124c0e534
Artur Mroz <frost47@gmail.com>
2018-12-21 13:33:16 +0100

    added Dominoes in C#

---
+++ csharp/dominoes/Dominoes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Dominoes
+{
+    public static bool CanChain(IEnumerable<(int, int)> dominoes)
+    {
+        if (!dominoes.Any()) return true;
+
+        var available = new List<(int, int)>(dominoes);
+        var chain = new List<(int, int)>();
+
+        var firstStone = available.First();
+        available.Remove(firstStone);
+        chain.Add(firstStone);
+
+        return Backtrack();
+
+        bool Backtrack()
+        {
+            if (!available.Any())
+            {
+                return chain.First().Item1 == chain.Last().Item2;
+            }
+
+            foreach (var stone in available.ToList())
+            {
+                available.Remove(stone);
+
+                if (chain.Last().Item2 == stone.Item1)
+                {
+                    chain.Add(stone);
+
+                    if (Backtrack()) return true;
+
+                    chain.Remove(stone);
+                }
+                else if (chain.Last().Item2 == stone.Item2)
+                {
+                    var reversed = (stone.Item2, stone.Item1);
+                    chain.Add(reversed);
+
+                    if (Backtrack()) return true;
+
+                    chain.Remove(reversed);
+                }
+
+                available.Add(stone);
+            }
+
+            return false;
+        }
+    }
+}
