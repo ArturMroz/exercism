10b96cc3634a2b9a829a08916b183118f90f1d7c
Artur Mroz <frost47@gmail.com>
2019-01-03 17:08:01 +0100

    added a few solutions in F#

---
+++ fsharp/grains/Grains.fs
@@ -0,0 +1,16 @@
+﻿module Grains
+
+let square (n: int): Result<uint64,string> =
+    match n with
+    | _ when 1 <= n && n <= 64 -> 
+        Ok (1UL <<< (n - 1))
+    | _ -> 
+        Error "square must be between 1 and 64"
+
+let total: Result<uint64,string> = 
+    [1..64] 
+    |> List.map square 
+    |> List.reduce (fun a b -> 
+        match a, b with
+        | Ok x, Ok y -> Ok (x + y)
+        | _ -> Error "Invalid input")
---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,10 @@
+﻿module Hamming
+
+let distance (strand1 : string) (strand2 : string) : int option =
+    match String.length strand1 <> String.length strand2 with
+    | true -> None
+    | false -> 
+        Seq.zip strand1 strand2
+        |> Seq.filter (fun (x, y) -> x <> y)
+        |> Seq.length
+        |> Some
