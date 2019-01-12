fd7a0f6c0b76d47042f4bf2439938bfa81cb70b4
Artur Mroz <frost47@gmail.com>
2019-01-12 02:53:21 +0000

    added 3 solutions in F#

---
+++ fsharp/high-scores/HighScores.fs
@@ -0,0 +1,15 @@
+﻿module HighScores
+
+let scores (values: int list): int list = values
+
+let latest (values: int list): int = List.last values
+
+let highest (values: int list): int = List.max values
+
+let top (values: int list): int list = values |> List.sortDescending |> List.truncate 3
+
+let report (values: int list): string = 
+    let latest, highest = latest values, highest values
+    match highest - latest with
+    | 0 -> sprintf "Your latest score was %i. That's your personal best!" latest
+    | d -> sprintf "Your latest score was %i. That's %i short of your personal best!" latest d
---
+++ fsharp/space-age/SpaceAge.fs
@@ -0,0 +1,25 @@
+﻿module SpaceAge
+
+type Planet =
+    | Earth
+    | Mercury
+    | Venus
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
+
+let age (planet: Planet) (seconds: int64): float =
+    let secondsInYear = 60.0 * 60.0 * 24.0 * 365.25
+    let earthYears = float seconds / secondsInYear
+
+    match planet with
+    | Earth     -> earthYears
+    | Mercury   -> earthYears / 0.2408467
+    | Venus     -> earthYears / 0.61519726
+    | Mars      -> earthYears / 1.8808158
+    | Jupiter   -> earthYears / 11.862615
+    | Saturn    -> earthYears / 29.447498
+    | Uranus    -> earthYears / 84.016846
+    | Neptune   -> earthYears / 164.79132
