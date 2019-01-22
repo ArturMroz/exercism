6cfa9747b3011cca3be9c13ae5bdc7251632b03c
Artur Mroz <frost47@gmail.com>
2019-01-22 18:38:23 +0000

    added a few solutions in F#

---
+++ fsharp/matrix/Matrix.fs
@@ -0,0 +1,11 @@
+﻿module Matrix
+
+let parse (matrix : string) =
+    matrix.Split '\n'
+    |> Array.map (fun row -> row.Split ' ' |> Array.map int)
+
+let row index matrix =
+    (parse matrix).[index] |> Array.toList
+
+let column index matrix =
+    [ for row in parse matrix -> row.[index] ]
---
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -0,0 +1,12 @@
+﻿module NucleotideCount
+
+let nucleotideCounts (strand : string) : Option<Map<char, int>> =
+    let nucleotides = [| 'A'; 'C'; 'G'; 'T' |]
+
+    if strand |> Seq.exists (fun ch -> not <| Array.contains ch nucleotides) then
+        None
+    else
+        nucleotides
+        |> Array.map (fun ch -> ch, strand |> Seq.filter ((=) ch) |> Seq.length)
+        |> Map.ofArray
+        |> Some
