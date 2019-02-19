1805dd333770549857df7e3a7b6d682a43aa6c52
Artur Mroz <frost47@gmail.com>
2019-02-19 22:19:44 +0000

    added DndCharacter in C#

---
+++ csharp/dnd-character/DndCharacter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+public class DndCharacter
+{
+    public int Strength { get; }
+    public int Dexterity { get; }
+    public int Constitution { get; }
+    public int Intelligence { get; }
+    public int Wisdom { get; }
+    public int Charisma { get; }
+    public int Hitpoints { get; }
+
+    static readonly Random _rnd = new Random();
+
+    public DndCharacter()
+    {
+        Strength = Ability();
+        Dexterity = Ability();
+        Constitution = Ability();
+        Intelligence = Ability();
+        Wisdom = Ability();
+        Charisma = Ability();
+        Hitpoints = 10 + Modifier(Constitution);
+    }
+
+    public static int Modifier(int score) => (int)Math.Floor((score - 10) / 2.0);
+
+    public static int Ability() =>
+        Enumerable
+            .Range(0, 4)
+            .Select(_ => _rnd.Next(1, 7))
+            .OrderBy(v => v)
+            .Skip(1)
+            .Sum();
+
+    public static DndCharacter Generate() => new DndCharacter();
+}
