9785ea29cb01122b6909c588f531a66f2ba927ec
Artur Mroz <frost47@gmail.com>
2019-03-15 23:26:45 +0000

    added DndCharacter in F#

---
+++ fsharp/dnd-character/DndCharacter.fs
@@ -0,0 +1,26 @@
+﻿module DndCharacter
+
+open System
+
+let modifier x =
+    (float (x - 10)) / 2.0
+    |> Math.Floor
+    |> int
+
+let ability() =
+    let rnd = Random()
+
+    [ 1..4 ]
+    |> List.map (fun _ -> rnd.Next(1, 7))
+    |> List.sort
+    |> List.skip 1
+    |> List.sum
+
+type DndCharacter() =
+    member val Strength = ability()
+    member val Dexterity = ability()
+    member val Constitution = ability()
+    member val Intelligence = ability()
+    member val Wisdom = ability()
+    member val Charisma = ability()
+    member this.Hitpoints with get() = 10 + modifier(this.Constitution)
