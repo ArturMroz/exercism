b74b2f9c80d5a4f64c6f5693d36edebf7e90b4f0
Artur Mroz <frost47@gmail.com>
2019-03-16 16:30:25 +0000

    added CircularBuffer in C#

---
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CircularBuffer<T>
+{
+    private readonly Queue<T> _buffer;
+    private readonly int _capacity;
+
+    public CircularBuffer(int capacity)
+    {
+        _capacity = capacity;
+        _buffer = new Queue<T>(capacity);
+    }
+
+    public T Read() => _buffer.Dequeue();
+
+    public void Write(T value)
+    {
+        if (_buffer.Count == _capacity) throw new InvalidOperationException();
+
+        _buffer.Enqueue(value);
+    }
+
+    public void Overwrite(T value)
+    {
+        if (_buffer.Count == _capacity) _buffer.Dequeue();
+
+        Write(value);
+    }
+
+    public void Clear() => _buffer.Clear();
+}
---
+++ csharp/circular-buffer/CircularBufferArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CircularBuffer_Array<T> where T : struct
+{
+    private readonly T?[] _buffer;
+    private int _current;
+
+    public CircularBuffer_Array(int capacity) => _buffer = new T?[capacity];
+
+    public T Read()
+    {
+        var result = SeekLast();
+
+        if (result == null) throw new InvalidOperationException();
+
+        return result.Value;
+    }
+
+    public void Write(T value)
+    {
+        if (_buffer[_current] != null) throw new InvalidOperationException();
+
+        Overwrite(value);
+    }
+
+    public void Overwrite(T value)
+    {
+        _buffer[_current] = value;
+        _current = GetNext(_current);
+    }
+
+    public void Clear()
+    {
+        SeekLast();
+    }
+
+    private int GetNext(int current) =>
+        current + 1 >= _buffer.Length ? 0 : current + 1;
+
+    private T? SeekLast()
+    {
+        var curr = _current;
+
+        do
+        {
+            var last = _buffer[curr];
+
+            if (last != null)
+            {
+                _buffer[curr] = null;
+                return last;
+            }
+
+            curr = GetNext(curr);
+        }
+        while (_current != curr);
+
+        return null;
+    }
+}
