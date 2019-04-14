e439f5be3eead25401ad38a68767fdda2cdce0fc
Artur Mroz <frost47@gmail.com>
2019-04-14 23:15:47 +0100

    added PascalsTriangle in F#

---
+++ fsharp/pascals-triangle/PascalsTriangle.fs
@@ -0,0 +1,21 @@
+﻿module PascalsTriangle
+
+
+let rows = function
+    | 0 -> []
+    | n ->
+        let nextRow curRow =
+            (0 :: curRow) @ [ 0 ]
+            |> List.windowed 2
+            |> List.map List.sum
+
+        [ 1..n - 1 ]
+        |> List.scan (fun acc _ -> nextRow acc) [ 1 ]
+
+
+let rows' n =
+    let createRow i =
+        [ 1..i - 1 ]
+        |> List.scan (fun acc j -> acc * (i - j) / j) 1
+
+    [ 1..n ] |> List.map createRow
