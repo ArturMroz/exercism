1b3f71c7325c813f240325a627812e11bbf3548a
Artur Mroz <frost47@gmail.com>
2020-04-06 19:46:47 +0100

    improved Armstrong Numbers

--- emacs-lisp/armstrong-numbers/armstrong-numbers.el
+++ emacs-lisp/armstrong-numbers/armstrong-numbers.el
@@ -6,8 +6,26 @@
 
 (require 'cl-lib)
 
+(defun get-digits (n)
+  "Get digits of N as a list, i.e. 123 => '(3 2 1)."
+  (if (zerop n)
+      '()
+    (cons (% n 10) (get-digits (/ n 10)))))
+
 (defun armstrong-p (number)
   "Check if NUMBER is an armstrong number."
+  (let* ((digits (get-digits number))
+         (number-of-digits (length digits))
+         (armstrong-sum
+          (reduce
+           (lambda (acc el)
+             (+ acc (expt el number-of-digits)))
+           digits
+           :initial-value 0)))
+    (= number armstrong-sum)))
+
+(defun armstrong-slow-p (number)
+  "Check if NUMBER is an armstrong number, use conversion to string."
   (let*
       ((stringified (number-to-string number))
        (number-of-digits (length stringified))
