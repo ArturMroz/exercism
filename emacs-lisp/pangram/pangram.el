c900e088210e95602fef328aa91bb2cd11302a33
Artur Mroz <frost47@gmail.com>
2020-04-09 19:31:05 +0100

    improved Pangram

--- emacs-lisp/pangram/pangram.el
+++ emacs-lisp/pangram/pangram.el
@@ -10,15 +10,15 @@
 (defun is-pangram (word)
   "Check if WORD is a pangram."
   (thread-last word
-    (downcase)
-    (seq-filter (lambda (c) (and (>= c ?a) (<= c ?z))))
-    (seq-uniq)
-    (seq-length)
+    downcase
+    (seq-filter (lambda (c) (<= ?a c ?z)))
+    seq-uniq
+    seq-length
     (= 26)))
 
 (defun is-pangram2 (word)
   "Check if WORD is a pangram."
-  (let ((alphabet "abcdefghijklmnopqrstuvwxyz")
+  (let ((alphabet (number-sequence ?a ?z))
         (downcased (downcase word)))
     (seq-every-p
      (apply-partially #'seq-contains-p downcased)
