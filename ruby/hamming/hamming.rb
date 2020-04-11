b3f11f9ab0915f0901b8111cadc3524719a9b293
Artur Mroz <frost47@gmail.com>
2020-04-11 20:47:13 +0100

    modified Hamming in Ruby

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,15 +1,12 @@
 class Hamming
   def self.compute(strand_a, strand_b)
     if strand_a.length != strand_b.length
-      raise ArgumentError, 'Strands have different length'
+      raise ArgumentError, 'Strands have different lengths'
     end
     
-    sum = 0
-    strand_a.split("").zip(strand_b.split("")).each{|x, y| sum += x != y ? 1 : 0}
-    sum
+    strand_a
+      .split("")
+      .zip(strand_b.split(""))
+      .sum { |x, y| x != y ? 1 : 0 }
   end
-end
-
-module BookKeeping
-  VERSION = 3 
 end
