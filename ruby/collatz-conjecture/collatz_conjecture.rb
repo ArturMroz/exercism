228df043668086ae1e338cc4c3ca86a1bf9b29f5
Artur Mroz <frost47@gmail.com>
2020-04-20 21:18:45 +0100

    add Collatz in Ruby

---
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -0,0 +1,13 @@
+class CollatzConjecture
+  def self.steps(number)
+    raise ArgumentError unless number.positive?
+
+    counter = 0
+    while number != 1
+      number = number.even? ? number / 2 : 3 * number + 1
+      counter += 1
+    end
+
+    counter
+  end
+end
