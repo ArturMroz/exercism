835ce4e660a5e79b670dd37c6d183b7d3cea1230
Artur Mroz <frost47@gmail.com>
2020-04-21 20:14:35 +0100

    more idiomatic TS

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,28 +1,40 @@
+type Color =
+  | 'black'
+  | 'brown'
+  | 'red'
+  | 'orange'
+  | 'yellow'
+  | 'green'
+  | 'blue'
+  | 'violet'
+  | 'grey'
+  | 'white'
+
+const colorsMap: { [key in Color]: number } = {
+  'black'      : 0,
+  'brown'      : 1,
+  'red'        : 2,
+  'orange'     : 3,
+  'yellow'     : 4,
+  'green'      : 5,
+  'blue'       : 6,
+  'violet'     : 7,
+  'grey'       : 8,
+  'white'      : 9,
+}
+
 export class ResistorColor {
-  private colors: string[];
+  private colors: Color[];
 
-  private allColors: string[] = [
-    "black",
-    "brown",
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "violet",
-    "grey",
-    "white",
-  ];
-
-  constructor(colors: string[]) {
+  constructor(colors: Color[]) {
     if (colors.length < 2)
-      throw "At least two colors need to be present"
+      throw 'At least two colors need to be present'
 
     this.colors = colors.slice(0, 2);
   }
 
   value = (): number => {
-    return this.allColors.indexOf(this.colors[0]) * 10
-      + this.allColors.indexOf(this.colors[1]);
+    const [first, second] = this.colors;
+    return colorsMap[first] * 10 + colorsMap[second];
   };
 }
