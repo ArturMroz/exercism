fc914432131b41f44a7ec657996c88111682b49c
Artur Mroz <frost47@gmail.com>
2020-05-21 22:10:22 +0100

    extract heavy lifting out of initialize

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,11 +1,12 @@
 class Phrase
-  attr_reader :word_count
-
   def initialize(phrase)
-    @word_count =
-      phrase
-        .downcase
-        .scan(/\d+|\w+'?\w+/)
-        .each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1 }
+    @phrase = phrase
+  end
+
+  def word_count
+    @phrase
+      .downcase
+      .scan(/\d+|\w+'?\w+/)
+      .each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1 }
   end
 end
