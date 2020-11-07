a6936ff23e08409e99b413f4f126550b3bbf2306
Artur Mroz <frost47@gmail.com>
2020-11-07 17:51:59 +0000

    modify Clock to store time as minutes only

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,38 +1,31 @@
 use std::fmt;
 
+const MINUTES_IN_HOUR: i32 = 60;
+const MINUTES_IN_DAY: i32 = 24 * MINUTES_IN_HOUR;
+
 #[derive(PartialEq, Debug)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let overflow_hours = minutes / 60;
+        let mut _minutes = (hours * MINUTES_IN_HOUR + minutes) % MINUTES_IN_DAY;
 
-        let mut _minutes = minutes % 60;
-        let mut _hours = (hours + overflow_hours) % 24;
-
-        if _minutes < 0 {
-            _minutes += 60;
-            _hours -= 1;
-        }
-
-        if _hours < 0 { _hours += 24 }
+        if _minutes < 0 { _minutes += MINUTES_IN_DAY };
 
         Self {
-            hours: _hours,
             minutes: _minutes,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock::new(self.hours, self.minutes + minutes)
+        Clock::new(0, self.minutes + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(f, "{:02}:{:02}", self.minutes / 60, self.minutes % 60)
     }
 }
