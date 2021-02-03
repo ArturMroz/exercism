d629c81a8ac38a387870d8bfe2800d4d31724cd4
Artur Mroz <frost47@gmail.com>
2021-02-03 20:46:44 +0000

    simplify Microwave

--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -1,14 +1,9 @@
 class Microwave
-  def initialize(seconds)
-    @seconds =
-      if seconds < 100
-        seconds
-      else
-        seconds.modulo(100) + seconds.div(100) * 60
-      end
-  end
+  attr_reader :timer
 
-  def timer
-    format('%02d:%02d', @seconds.div(60), @seconds.modulo(60))
+  def initialize(input)
+    seconds = input.modulo(100) + input.div(100) * 60
+
+    @timer = format('%02d:%02d', *seconds.divmod(60))
   end
 end
