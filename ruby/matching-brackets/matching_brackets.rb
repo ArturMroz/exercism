cbc106143e78d76c08e7f15ab5cea7cc4f8bba7e
Artur Mroz <frost47@gmail.com>
2021-02-12 22:18:39 +0000

    make Brackets more readable

--- ruby/matching-brackets/matching_brackets.rb
+++ ruby/matching-brackets/matching_brackets.rb
@@ -1,13 +1,19 @@
 class Brackets
-  PAIRS = { '{' => '}', '(' => ')', '[' => ']' }.freeze
+  PAIRS = {
+    '{' => '}',
+    '(' => ')',
+    '[' => ']'
+  }.freeze
 
   def self.paired?(input)
-    input.chars.each_with_object([]) do |ch, stack|
-      if PAIRS.keys.include?(ch)
-        stack.push(PAIRS[ch])
-      elsif PAIRS.values.include?(ch)
-        return false unless ch == stack.pop
-      end
-    end.empty?
+    input
+      .chars
+      .each_with_object([]) do |ch, stack|
+        if PAIRS.keys.include? ch
+          stack.push PAIRS[ch]
+        elsif PAIRS.values.include? ch
+          return false unless ch == stack.pop
+        end
+      end.empty?
   end
 end
