ddf70441d6d6b6ecda21c19f13fca3bcb8af764c
Artur Mroz <frost47@gmail.com>
2021-03-07 21:03:08 +0000

    add Bob in Clojure

---
+++ clojure/bob/src/bob.clj
@@ -0,0 +1,17 @@
+(ns bob
+  (:require [clojure.string :as str]))
+
+(defn- shout? [s]
+  (and (= s (str/upper-case s))
+       (re-find #"[a-zA-Z]" s)))
+
+(defn- question? [s]
+  (str/ends-with? (str/trimr s) "?"))
+
+(defn response-for [s]
+  (cond
+    (and (shout? s) (question? s)) "Calm down, I know what I'm doing!"
+    (shout? s) "Whoa, chill out!"
+    (question? s)"Sure."
+    (str/blank? s) "Fine. Be that way!"
+    :else "Whatever."))
