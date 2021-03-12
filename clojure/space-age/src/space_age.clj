679c5944a28a29430fc02035f57a900b9261198b
Artur Mroz <frost47@gmail.com>
2021-03-12 22:50:22 +0000

    use macro to solve Space Age

--- clojure/space-age/src/space_age.clj
+++ clojure/space-age/src/space_age.clj
@@ -1,25 +1,22 @@
 (ns space-age)
 
-(defn on-earth [seconds]
-  (/ seconds (* 365.25 24 60 60)))
+(def earth-orbit-in-seconds 31557600)
 
-(defn on-mercury [seconds]
-  (/ (on-earth seconds) 0.2408467))
+(def orbital-periods
+  {"mercury" 0.2408467
+   "venus"   0.61519726
+   "earth"   1
+   "mars"    1.8808158
+   "jupiter" 11.862615
+   "saturn"  29.447498
+   "uranus"  84.016846
+   "neptune" 164.79132})
 
-(defn on-venus [seconds]
-  (/ (on-earth seconds) 0.61519726))
+(defmacro create-orbital-fns []
+  (let [fns (for [[planet period] orbital-periods]
+              `(defn ~(symbol (str "on-" planet))
+                 [seconds#]
+                 (/ seconds# earth-orbit-in-seconds ~period)))]
+    `(do ~@fns)))
 
-(defn on-mars [seconds]
-  (/ (on-earth seconds) 1.8808158))
-
-(defn on-jupiter [seconds]
-  (/ (on-earth seconds) 11.862615))
-
-(defn on-saturn [seconds]
-  (/ (on-earth seconds) 29.447498))
-
-(defn on-uranus [seconds]
-  (/ (on-earth seconds) 84.016846))
-
-(defn on-neptune [seconds]
-  (/ (on-earth seconds) 164.79132))
+(create-orbital-fns)
