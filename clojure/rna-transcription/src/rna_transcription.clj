b059ddfee577faa69d443dcc237a619f3abeadf1
Artur Mroz <frost47@gmail.com>
2021-03-18 20:35:48 +0000

    simplify RNA Transcription

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,13 +1,13 @@
 (ns rna-transcription)
 
-(def complements
+(def rna->dna
   {\G \C
    \C \G
    \T \A
    \A \U})
 
 (defn to-rna [dna]
-  (let [translated (map #(get complements %) dna)]
+  (let [translated (map rna->dna dna)]
     (if (some nil? translated)
       (throw (AssertionError. "Invalid strand"))
       (apply str translated))))
