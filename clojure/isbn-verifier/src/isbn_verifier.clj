2ad90933d3f9ca9ab6187ba3deb10c740cae1a5c
Artur Mroz <frost47@gmail.com>
2021-04-08 21:16:22 +0100

    add ISBN Verifier in Clojure

---
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -0,0 +1,23 @@
+(ns isbn-verifier)
+
+(defn- valid-format? [isbn]
+  (re-matches #"\d-?\d{3}-?\d{5}-?[\dX]" isbn))
+
+(defn- char->digit [ch]
+  (if (= ch \X)
+    10
+    (Character/digit ch 10)))
+
+(defn- divisible-by? [divisor n]
+  (zero? (mod n divisor)))
+
+(defn- valid-checksum? [digits]
+  (->> digits
+       (remove #(= % \-))
+       (map char->digit)
+       (map * (range 10 0 -1))
+       (apply +)
+       (divisible-by? 11)))
+
+(defn isbn? [isbn]
+  ((every-pred valid-format? valid-checksum?) isbn))
