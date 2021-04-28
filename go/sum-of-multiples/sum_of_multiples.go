184ae2e30bcdb5fcde271cfebee5b5a14bc23b04
Artur Mroz <frost47@gmail.com>
2021-04-28 21:53:43 +0100

    name return variable

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -2,8 +2,7 @@ package summultiples
 
 // SumMultiples finds the sum of all the unique multiples of particular numbers
 // up to but not including the given number.
-func SumMultiples(limit int, divisor ...int) int {
-	sum := 0
+func SumMultiples(limit int, divisor ...int) (sum int) {
 	for i := 1; i < limit; i++ {
 		for _, d := range divisor {
 			if d == 0 {
