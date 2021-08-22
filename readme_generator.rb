7dc636588b54e0fd3f398c0821adcac65e2f0387
Artur Mroz <frost47@gmail.com>
2021-08-22 17:49:57 +0100

    added Stats section to readme_generator

--- readme_generator.rb
+++ readme_generator.rb
@@ -1,4 +1,4 @@
-text = %q(
+text = %(
 # Exercism.io solutions
 
 > You should not have a favourite weapon. To become over-familiar with one weapon is as much a fault as not knowing it sufficiently well.
@@ -6,16 +6,9 @@ text = %q(
 > ― Miyamoto Musashi, The Book of Five Rings
 
 My solutions to [exercism.io](https://exercism.io) problems using various languages. For fun and profit.
-
-## Solved problems
-
-This table lists solved problems and conviniently links to the solutions in different languages, making it easy to compare the implementations.
-
-| Problem name | Languages |
-| --- | --- |
 )
 
-fmt_lang = {
+langs_fmt = {
   'clojure'    => 'Clojure',
   'csharp'     => 'C#',
   'elm'        => 'Elm',
@@ -29,16 +22,42 @@ fmt_lang = {
   'typescript' => 'TypeScript',
 }
 
+all_solutions = Dir['*/*']
 problems_hash = Hash.new { [] }
+langs_count = Hash.new(0)
 
-Dir['*/*'].each do |dir|
+all_solutions.each do |dir|
   lang, problem = dir.split('/')
   problems_hash[problem] = problems_hash[problem].push(lang)
+  langs_count[lang] += 1
 end
+
+text += %(
+## Stats
+
+Total: #{all_solutions.size} solutions, #{problems_hash.size} unique problems, #{langs_fmt.size} languages.
+
+| Language | No of solutions | % of total solutions |
+| --- | --- | --- |
+)
+
+langs_count.sort_by { |_, v| -v }.each do |lang, count|
+  total_percent = (count.to_f / all_solutions.size * 100).round(1)
+  text += "| [#{langs_fmt[lang]}](#{lang}) | #{count} | #{total_percent}% |\n"
+end
+
+text += %(
+## Solved problems
+
+List of solved problems with links to the solutions in different languages (so it's easy to compare the implementations).
+
+| Problem name | Languages |
+| --- | --- |
+)
 
 problems_hash.sort.each do |problem, langs|
   problem_name = problem.split('-').map(&:capitalize).join(' ')
-  links = langs.sort.map { |l| "[#{fmt_lang[l]}](#{l}/#{problem})" }.join(', ')
+  links = langs.sort.map { |l| "[#{langs_fmt[l]}](#{l}/#{problem})" }.join(', ')
 
   text += "| #{problem_name} | #{links} |\n"
 end
